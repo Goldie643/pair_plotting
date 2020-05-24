976d1e26e0246642b4a586a838475d4dde4ff534
Alex Goldsack <alex.goldsack1@gmail.com>
2020-05-25 04:32:33 +0900

    Started work and planned on how to plot this properly.

--- plot_pairs2.py
+++ plot_pairs2.py
@@ -8,44 +8,57 @@ import math
 import sys
 import argparse
 
-date_start = "2016-10-01"
-date_end = "2018-04-01"
+date_start = "2016-10"
+date_end = "2018-04"
 
 pre_th_start = date_start
-pre_th_end = "2017-06-01"
+pre_th_end = "2017-06"
 
-post_th_start = "2017-07-01"
+post_th_start = "2017-07"
 post_th_end = date_end
 
 lf_file = "n_int_20200211-094023.csv"
 
+bar_width = 0.25
 
 def main():
     parser = argparse.ArgumentParser(description="Plot IBD pair rates.")
-    parser.add_argument("file_names", metavar="Pair csvs", type=str, nargs="+",
-        help=".csv viles of format run,tot,pairs of IBD pairs per run")
-    parser.add_argument("--monthly", dest="plot_pairs", action="store_const",
-        const=plot_monthly, default=plot_periods,
-        help="Plot pair rate monthly (default: plot in distinct periods)")
+    parser.add_argument("file", metavar="Pair csv", type=str,
+        help=".csv file of format run,tot,pairs of IBD pairs per run")
+    parser.add_argument("--unblind", type=str, 
+        default=pre_th_start+"-"+pre_th_end,
+        help="Period to extrapolate from in YYYY-MM_YYYY-MM format.")
+    parser.add_argument("--blind", type=str,
+        default=post_th_start+"-"+post_th_end,
+        help="Period to extrapolate to in YYYY-MM_YYYY-MM format.")
+    # parser.add_argument("--monthly", dest="plot_pairs", action="store_const",
+    #     const=plot_monthly, default=plot_periods,
+    #     help="Plot pair rate monthly (default: plot in distinct periods)")
 
     args = parser.parse_args()
+
     fig, ax1 = plt.subplots()
 
-    for file_name in args.file_names:
-        args.plot_pairs(file_name,ax1)
+    # Select only unblinded period in pairs
+    # Get total pairs in period from tot column
+    # Normalise (/week might be nice) using run info
+    # Get same normalised number from skreact info
+    # Minus off from total, you now have raw BG rate
+    # and signal rate
+    # Extrapolate to post: use same BG rate, add on 
+    # new skreact rate
+    # Get bg and signal efficiency from traditional cuts/CNN
+    # Multiply the pre/post bg/signal rates by this to get pre/post
+    # cut rates.
 
-    # lf_skreact = pd.read_csv("lf_20191107-005242.csv")
+    pre_period = (args.unblind[:7],args.unblind[-7:])
+    post_period = (args.blind[:7],args.blind[-7:])
+
     lf_skreact = pd.read_csv(lf_file)
     lf_skreact["month"] = lf_skreact["month"].apply(
         lambda month : dt.strptime(month, "%Y-%m"))
     lf_skreact.set_index("month",inplace=True)
-    # ax2 = ax1.twinx()
-    # lf_skreact.loc[date_start:date_end,"n_int"].plot(
-    #     ax=ax1,label="Expected SK Interactions")
-    # ax1.legend(loc="upper left")
-    # ax2.legend(loc="upper center")
     ax1.set_ylabel(r"N Pairs [month$^{-1}$]")
-    # ax2.set_ylabel(r"N Reduced Pair Candidates [month$^{-1}$]")
     ax1.set_xlabel("")
     ax1.set_ylim(ymin=0)
 
@@ -58,75 +71,6 @@ def month_scale(date, pairs):
     year = date.year
     month = date.month
     return monthrange(year,month)[1]*24*60*60*pairs
-
-# Groups pairs monthly, scaled to livetime
-def plot_monthly(file_name, ax):
-    raw_pairs = pd.read_csv(file_name)
-    run_info = pd.read_csv("run_info.csv",index_col=0)
-    runs = []
-    pairs = []
-    run_dur = []
-    dates_unix = []
-    last_run = int(raw_pairs["run"][0])
-    run_pairs = 0
-
-    # Adding up runs' pair counts
-    for row in raw_pairs.itertuples():
-        if(row.run == last_run):
-            run_pairs += row.pairs
-        else:
-            # run_pairs = row.pairs
-            runs.append(last_run)
-            run_dur.append(run_info.loc[last_run]["time_diff"])
-            pairs.append(run_pairs)
-            dates_unix.append(run_info.loc[last_run]["start_time"])
-            run_pairs = row.pairs
-            last_run = int(row.run)
-
-    # Assume all runs are entirely contained in the start date. TODO: fix this.
-    dates = [dt.fromtimestamp(date_unix) for date_unix in dates_unix]
-
-    pairs_df = pd.DataFrame({ 
-        "run": runs,
-        "pairs": pairs,
-        "run_dur": run_dur,
-        "date_unix": dates_unix,
-        "date": dates })
-
-    # Get rid of short runs
-    # pairs_df = pairs_df[pairs_df["run_dur"] > 80000]
-
-    freq="M"
-    # Sum of pairs in a month
-    grouped_pairs = pairs_df.groupby(
-        [pd.Grouper(key="date", freq=freq)])["pairs"].sum()
-    # Sum of run time in a month
-    grouped_run_dur = pairs_df.groupby(
-        [pd.Grouper(key="date", freq=freq)])["run_dur"].sum()
-    # Get pairs/s from this
-    grouped_pairs_err = grouped_pairs.apply(np.sqrt)
-    normed_pairs = grouped_pairs.div(grouped_run_dur)
-    normed_pairs_err = grouped_pairs_err.div(grouped_run_dur)
-    normed_pairs.interpolate(inplace=True)
-
-    normed_month_pairs_ls = []
-    normed_month_errors_ls = []
-
-    for date, pairs in normed_pairs.iteritems():
-        normed_month_pairs_ls.append(month_scale(date, pairs))
-
-    for date, err in normed_pairs_err.iteritems():
-        normed_month_errors_ls.append(month_scale(date, err))
-
-    normed_month_pairs = pd.Series(
-        normed_month_pairs_ls, index = normed_pairs.index)
-    normed_month_errors = pd.Series(
-        normed_month_errors_ls, index = normed_pairs.index)
-
-    normed_month_pairs.loc[date_start:date_end].plot(
-        yerr=normed_month_errors.loc[date_start:date_end],
-        ax=ax,style="-",label=file_name,capsize=5)
-    return
 
 def plot_periods(file_name, ax):
     # raw_pairs = pd.read_csv("pairs_sol_cut_extra_tight_fv.csv")
